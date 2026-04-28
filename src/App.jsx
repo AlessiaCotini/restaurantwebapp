3afd529c01692a5aa91ac278c38f168c192a652e
AlessiaCotini <alessia.cotini@icloud.com>
2026-04-28 10:18:29 +0200

    delete file css add import bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import './App.css'
+//importo dalle dipendenze
+//questa riga ha importato il foglio css di bootstrap in TUTTO IL PROGETTO
+import 'bootstrap/dist/css/bootstrap.min.css'
 // mostra i contenuti iniziali 
 
 function App() {
