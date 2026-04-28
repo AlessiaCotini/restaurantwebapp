7764e8264607e5cfd2073300096ece60de0355a4
AlessiaCotini <alessia.cotini@icloud.com>
2026-04-28 11:27:10 +0200

    navbar named

--- components/RestNavbar.jsx
+++ components/RestNavbar.jsx
@@ -1,43 +1,33 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
+import NavDropdown from 'react-bootstrap/NavDropdown';
 function RestNavbar () {
   return (
     <>
-      <Navbar bg="dark" data-bs-theme="dark" expand='md'>
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+      <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Dalla Sora Lella</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#home">Menù</Nav.Link>
+            <Nav.Link href="#link">Dove trovarci</Nav.Link>
+            <NavDropdown title="Contatti" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Chiama</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Messaggia
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Prenota</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Gift Card
+              </NavDropdown.Item>
+            </NavDropdown>
           </Nav>
-        </Container>
-      </Navbar>
-      <br />
-      <Navbar bg="primary" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-
-      <br />
-      <Navbar bg="light" data-bs-theme="light">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     </>
   );
 }
