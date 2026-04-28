6c9f4bff56f91d139bb216ab1d38179cd5c44483
AlessiaCotini <alessia.cotini@icloud.com>
2026-04-28 10:32:30 +0200

    add react-bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 //importo dalle dipendenze
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 //questa riga ha importato il foglio css di bootstrap in TUTTO IL PROGETTO
-// mostra i contenuti iniziali 
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
