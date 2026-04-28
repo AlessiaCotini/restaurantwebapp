2d05429941b3dc8ab0be99e960142c01f0b4aea2
AlessiaCotini <alessia.cotini@icloud.com>
2026-04-28 10:19:01 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 //importo dalle dipendenze
-//questa riga ha importato il foglio css di bootstrap in TUTTO IL PROGETTO
+
 import 'bootstrap/dist/css/bootstrap.min.css'
+//questa riga ha importato il foglio css di bootstrap in TUTTO IL PROGETTO
 // mostra i contenuti iniziali 
 
 function App() {
