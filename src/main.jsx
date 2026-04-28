0d633401cd08c6b66c9017089ab556aa333d17ec
AlessiaCotini <alessia.cotini@icloud.com>
2026-04-28 09:39:03 +0200

    react library

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+//crea un elemento radice e ci ignetta dentro il componente app che da via a tutta l'applicazione
+//modalità di sviluppo che crea messaggi di bug non sempre utili e/o necessari
+
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <App />
-  </StrictMode>,
 )
