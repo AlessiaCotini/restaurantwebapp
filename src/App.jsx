7ed8148e6308444e99d4898eaeef462d968da22b
AlessiaCotini <alessia.cotini@icloud.com>
2026-04-28 10:35:48 +0200

    import container row col

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
 //importo dalle dipendenze
-
 import 'bootstrap/dist/css/bootstrap.min.css'
 //questa riga ha importato il foglio css di bootstrap in TUTTO IL PROGETTO
-
+import { Container, Row, Col  } from 'react-bootstrap'
 
 function App() {
 
   return (
     <>
-      
+    <Container className=''>
+      <Row className=''>
+        <Col className=''>
+        </Col>
+      </Row>
+    </Container>
     </>
   )
 }
