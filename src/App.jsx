1040bf908aeb982a0507b6fea0d2be51009528cb
AlessiaCotini <alessia.cotini@icloud.com>
2026-04-28 10:37:25 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 //questa riga ha importato il foglio css di bootstrap in TUTTO IL PROGETTO
 import { Container, Row, Col  } from 'react-bootstrap'
-
+//graffe FONDAMENTALI perchè NON SONO DI DEFAULT
+//i componenti che dichiariamo noi saranno UNO per file quindi li si possono esportare come default quindi importre senza graffe
 function App() {
 
   return (
