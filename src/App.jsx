9a57d0476757ea6845f7b212f1e2a5bf7b1f8d1c
AlessiaCotini <alessia.cotini@icloud.com>
2026-04-28 11:06:21 +0200

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
 //importo dalle dipendenze
 import 'bootstrap/dist/css/bootstrap.min.css'
 //questa riga ha importato il foglio css di bootstrap in TUTTO IL PROGETTO
-import { Container, Row, Col  } from 'react-bootstrap'
+import { Container, Row, Col,   } from 'react-bootstrap'
 //graffe FONDAMENTALI perchè NON SONO DI DEFAULT
 //i componenti che dichiariamo noi saranno UNO per file quindi li si possono esportare come default quindi importre senza graffe
+import RestNavbar from '../components/RestNavbar'
 function App() {
 
   return (
     <>
+    <header>
+    <RestNavbar />
+    </header>
+    <main>
     <Container className=''>
       <Row className=''>
-        <Col className=''>
+        <Col className='col-12'>
+        
+
         </Col>
       </Row>
     </Container>
+    </main>
+    <footer>
+      
+    </footer>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 //crea un elemento radice e ci ignetta dentro il componente app che da via a tutta l'applicazione
 //modalità di sviluppo che crea messaggi di bug non sempre utili e/o necessari
