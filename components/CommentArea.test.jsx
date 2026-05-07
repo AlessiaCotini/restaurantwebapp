9878436c814a8483856b3e5c63ff37e5d5f7f2bb
AlessiaCotini <alessia.cotini@icloud.com>
2026-05-07 16:24:25 +0200

    Update CommentArea.test.jsx

--- components/CommentArea.test.jsx
+++ components/CommentArea.test.jsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import CommentArea from "./CommentArea";
 import App from "../src/App";
@@ -11,7 +11,7 @@ describe("Render CommentArea", () => {
   });
   it("finds comment on click", async () => {
     render(<App />, <SingleBook />);
-    const allBooks = screen.getAllByTestId("search");
+    const allBooks = screen.getByTestId("search");
     fireEvent.click(allBooks[0]);
     const comment = await screen.findAllByTestId("comment");
     expect(comment.length).toBeGreaterThan(0);
